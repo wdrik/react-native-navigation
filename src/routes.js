1e825c128af23c538b62421fdc2fec7ea347f6e5
iosilva <iosilva@I4PRO08>
2019-11-13 12:22:40 -0300

    BottomTabNavigator

--- src/routes.js
+++ src/routes.js
@@ -62,29 +62,44 @@
 // export default createAppContainer(mainNavigation);
 
 // ***DrawerNavigator                --------------------------------------------------------------------------------
-import React from "react";
-import { createAppContainer } from "react-navigation";
-import { createDrawerNavigator } from "react-navigation-drawer";
-import Icon from "react-native-vector-icons/FontAwesome";
+// import React from "react";
+// import { createAppContainer } from "react-navigation";
+// import { createDrawerNavigator } from "react-navigation-drawer";
+// import Icon from "react-native-vector-icons/FontAwesome";
 
-Icon.loadFont();
+// Icon.loadFont();
+
+// import Home from "./pages/Home";
+// import Teste from "./pages/Teste";
+
+// const mainNavigation = createDrawerNavigator({
+//   Home: {
+//     screen: Home,
+//     navigationOptions: () => ({
+//       drawerIcon: <Icon name="lock" size={20} color="#Da552f" />
+//     })
+//   },
+//   Teste: {
+//     screen: Teste,
+//     navigationOptions: () => ({
+//       drawerIcon: <Icon name="rocket" size={20} color="#Da552f" />
+//     })
+//   }
+// });
+
+// export default createAppContainer(mainNavigation);
+
+// ***BottomTabNavigator                --------------------------------------------------------------------------------
+import { createAppContainer } from "react-navigation";
+
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
 import Home from "./pages/Home";
 import Teste from "./pages/Teste";
 
-const mainNavigation = createDrawerNavigator({
-  Home: {
-    screen: Home,
-    navigationOptions: () => ({
-      drawerIcon: <Icon name="lock" size={20} color="#7159c1" />
-    })
-  },
-  Teste: {
-    screen: Teste,
-    navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#7159c1" />
-    })
-  }
+const mainNavigation = createBottomTabNavigator({
+  Home,
+  Teste
 });
 
 export default createAppContainer(mainNavigation);
