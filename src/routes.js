574242cb390e8fec2aa440f8e7c02381d47e43eb
iosilva <iosilva@I4PRO08>
2019-11-13 11:58:18 -0300

    createAnimatedSwitchNavigator and StackNavigator

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+// ***createSwitchNavigator         --------------------------------------------------------------------------------
 // import { createSwitchNavigator, createAppContainer } from "react-navigation";
 
 // import Home from "./pages/Home";
@@ -15,31 +16,75 @@
 
 // export default createAppContainer(mainNavigation);
 
+// ***createAnimatedSwitchNavigator   --------------------------------------------------------------------------------
+// import React from "react";
+// import { createAppContainer } from "react-navigation";
+// import createAnimatedSwitchNavigator from "react-navigation-animated-switch";
+// import { Transition } from "react-native-reanimated";
+
+// import Home from "./pages/Home";
+// import Teste from "./pages/Teste";
+
+// const mainNavigation = createAnimatedSwitchNavigator(
+//   {
+//     Home,
+//     Teste
+//   },
+//   {
+//     transition: (
+//       <Transition.Together>
+//         <Transition.Out
+//           type="slide-top"
+//           durationMs={1000}
+//           interpolation="easeIn"
+//         />
+//         <Transition.In type="fade" durationMs={1000} />
+//       </Transition.Together>
+//     )
+//   }
+// );
+
+// export default createAppContainer(mainNavigation);
+
+// ***StackNavigator                --------------------------------------------------------------------------------
+// import { createAppContainer } from "react-navigation";
+
+// import { createStackNavigator } from "react-navigation-stack";
+
+// import Home from "./pages/Home";
+// import Teste from "./pages/Teste";
+
+// const mainNavigation = createStackNavigator({
+//   Home,
+//   Teste
+// });
+
+// export default createAppContainer(mainNavigation);
+
+// ***DrawerNavigator                --------------------------------------------------------------------------------
 import React from "react";
 import { createAppContainer } from "react-navigation";
-import createAnimatedSwitchNavigator from "react-navigation-animated-switch";
-import { Transition } from "react-native-reanimated";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+Icon.loadFont();
 
 import Home from "./pages/Home";
 import Teste from "./pages/Teste";
 
-const mainNavigation = createAnimatedSwitchNavigator(
-  {
-    Home,
-    Teste
+const mainNavigation = createDrawerNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: () => ({
+      drawerIcon: <Icon name="lock" size={20} color="#7159c1" />
+    })
   },
-  {
-    transition: (
-      <Transition.Together>
-        <Transition.Out
-          type="slide-bottom"
-          durationMs={400}
-          interpolation="easeIn"
-        />
-        <Transition.In type="fade" durationMs={500} />
-      </Transition.Together>
-    )
+  Teste: {
+    screen: Teste,
+    navigationOptions: () => ({
+      drawerIcon: <Icon name="rocket" size={20} color="#7159c1" />
+    })
   }
-);
+});
 
 export default createAppContainer(mainNavigation);
