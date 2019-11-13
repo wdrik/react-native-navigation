906c105b72025b2499afad60efd2527525478c20
iosilva <iosilva@I4PRO08>
2019-11-13 11:21:59 -0300

    createAnimatedSwitchNavigator

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,44 @@
-import { createSwitchNavigator, createAppContainer } from "react-navigation";
+// import { createSwitchNavigator, createAppContainer } from "react-navigation";
+
+// import Home from "./pages/Home";
+// import Teste from "./pages/Teste";
+
+// const mainNavigation = createSwitchNavigator(
+//   {
+//     Home,
+//     Teste
+//   },
+//   {
+//     initialRouteName: "Home"
+//   }
+// );
+
+// export default createAppContainer(mainNavigation);
+
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import createAnimatedSwitchNavigator from "react-navigation-animated-switch";
+import { Transition } from "react-native-reanimated";
 
 import Home from "./pages/Home";
 import Teste from "./pages/Teste";
 
-const mainNavigation = createSwitchNavigator(
+const mainNavigation = createAnimatedSwitchNavigator(
   {
     Home,
     Teste
   },
   {
-    initialRouteName: "Home"
+    transition: (
+      <Transition.Together>
+        <Transition.Out
+          type="slide-bottom"
+          durationMs={400}
+          interpolation="easeIn"
+        />
+        <Transition.In type="fade" durationMs={500} />
+      </Transition.Together>
+    )
   }
 );
 
